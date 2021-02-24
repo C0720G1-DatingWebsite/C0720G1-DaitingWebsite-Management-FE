84ad0d2dc6ffd71ff6a307e8f00cfe05e3bd45ad
winback1000 <68835331+winback1000@users.noreply.github.com>
2021-02-24 10:16:06 +0700

    Khoa update FE

--- c0720g1-dating/src/app/app.module.ts
+++ c0720g1-dating/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent
   ],
   imports: [
     BrowserModule,
