a825e9b7212cee6665d853623b7ca0f5d75dc9bb
trancongphuoc <68835221+trancongphuoc@users.noreply.github.com>
2021-02-24 17:09:52 +0700

    Update app.component.ts

--- c0720g1-dating/src/app/app.component.ts
+++ c0720g1-dating/src/app/app.component.ts
@@ -22,7 +22,24 @@ export class AppComponent implements OnInit, DoCheck {
 
   @ViewChild('element', {static: true }) loginElement: ElementRef;
 
-  constructor() {
+   constructor(private loadResourceService:LoadResourceService) {	
+     
+    this.loadResourceService.loadScript('assets/js/utils/app.js');	 
+    this.loadResourceService.loadScript('assets/js/utils/page-loader.js');	
+    this.loadResourceService.loadScript('assets/js/vendor/simplebar.min.js');	
+    this.loadResourceService.loadScript('assets/js/utils/liquidify.js');	
+    this.loadResourceService.loadScript('assets/js/vendor/xm_plugins.min.js');	
+    // this.loadResourceService.loadScript('assets/js/vendor/tiny-slider.min.js');	
+    this.loadResourceService.loadScript('assets/js/global/global.hexagons.js');	
+    this.loadResourceService.loadScript('assets/js/global/global.tooltips.js');	
+    this.loadResourceService.loadScript('assets/js/global/global.popups.js');	
+    this.loadResourceService.loadScript('assets/js/header/header.js');	
+    this.loadResourceService.loadScript('assets/js/sidebar/sidebar.js');	
+    this.loadResourceService.loadScript('assets/js/content/content.js');	
+    this.loadResourceService.loadScript('assets/js/form/form.utils.js');	
+    this.loadResourceService.loadScript('assets/js/utils/svg-loader.js');	
+
+    this.loadResourceService.loadScript('assets/js/global/global.accordions.js');	
   }
 
   ngOnInit(): void {
