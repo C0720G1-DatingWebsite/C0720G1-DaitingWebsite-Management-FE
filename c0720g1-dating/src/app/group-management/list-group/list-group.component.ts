cf7b08add23532ac2cdfdde12ff4093108c82f51
nvankhanh97 <nvankhanh97@gmail.com>
2021-02-26 08:48:02 +0700

    khanh 26/2

--- c0720g1-dating/src/app/group-management/list-group/list-group.component.ts
+++ c0720g1-dating/src/app/group-management/list-group/list-group.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadResourceService} from "../../load-resource.service";
+import {GroupService} from "../group.service";
+import {IGroup} from "../IGroup";
 
 @Component({
   selector: 'app-list-group',
@@ -6,10 +9,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-group.component.scss']
 })
 export class ListGroupComponent implements OnInit {
+  public listGroup: IGroup [];
+  page = 0;
+  pageable: any;
 
-  constructor() { }
+
+  constructor(private loadResourceService: LoadResourceService,
+              public groupService: GroupService) {
+    this.loadScript();
+  }
 
   ngOnInit(): void {
+    this.getListGroup()
+  }
+
+  getListGroup() {
+    this.groupService.getListGroup(this.page).subscribe(data => {
+      this.listGroup = data.content;
+      console.log(data.content)
+      this.pageable = data
+    })
+  }
+
+
+  loadScript() {
+    this.loadResourceService.loadScript('assets/js/utils/app.js');
+    this.loadResourceService.loadScript('assets/js/utils/page-loader.js');
+    this.loadResourceService.loadScript('assets/js/vendor/simplebar.min.js');
+    this.loadResourceService.loadScript('assets/js/utils/liquidify.js');
+    this.loadResourceService.loadScript('assets/js/vendor/xm_plugins.min.js');
+    this.loadResourceService.loadScript('assets/js/sidebar/sidebar.js');
+    this.loadResourceService.loadScript('assets/js/global/global.popups.js');
+    this.loadResourceService.loadScript('assets/js/form/form.utils.js');
+    this.loadResourceService.loadScript('assets/js/utils/svg-loader.js');
+    this.loadResourceService.loadScript('assets/js/global/global.accordions.js');
+    setTimeout(() => {
+      this.loadResourceService.loadScript('assets/js/global/global.hexagons.js');
+      this.loadResourceService.loadScript('assets/js/global/global.tooltips.js');
+      this.loadResourceService.loadScript('assets/js/header/header.js');
+      this.loadResourceService.loadScript('assets/js/content/content.js');
+      this.loadResourceService.loadScript('assets/js/vendor/tiny-slider.min.js');
+    }, 200)
   }
 
 }
