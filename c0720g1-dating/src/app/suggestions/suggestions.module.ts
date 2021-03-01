c0734d89dd08e4c4987e64178555316a35d8f0cc
Hoangtu9999 <58470170+Hoangtu9999@users.noreply.github.com>
2021-03-01 17:22:33 +0700

    tu up

--- c0720g1-dating/src/app/suggestions/suggestions.module.ts
+++ c0720g1-dating/src/app/suggestions/suggestions.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SuggestionToMakeFriendsComponent } from './suggestion-to-make-friends/suggestion-to-make-friends.component';
 import { SuggestedPairingComponent } from './suggested-pairing/suggested-pairing.component';
 import {SuggestionsRoutingModule} from "./suggestions-routing.module";
+import {InfiniteScrollModule} from "ngx-infinite-scroll";
 
 
 
@@ -10,7 +11,8 @@ import {SuggestionsRoutingModule} from "./suggestions-routing.module";
   declarations: [SuggestionToMakeFriendsComponent, SuggestedPairingComponent],
   imports: [
     CommonModule,
-    SuggestionsRoutingModule
+    SuggestionsRoutingModule,
+    InfiniteScrollModule
   ]
 })
 export class SuggestionsModule { }
